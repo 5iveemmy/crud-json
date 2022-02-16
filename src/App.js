cda38de11d2b8d24141b9eab4514d8a2defa1b65
5iveemmy <82128935+5iveemmy@users.noreply.github.com>
2022-02-16 23:13:15 +0100

    fetchData

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import FormValid from "./compoonent/FormValid";
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <FormValid />
-    </div>
-  );
+export default function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    await fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.log(error));
+  };
+
+  // const onAdd = aync (name, email) => {
+  //   await fetch("https://jsonplaceholder.typicode.com/users", {
+  //     method: 'POST',
+  //     body: JSON.stringify({
+  //       name: name,
+  //       email: email
+  //     }),
+  //     headers: {
+  //       "Content-type": "applcation/json; charset=UTF-8"
+  //     }
+  //   })
+  //     .then((response) => {
+
+  //     })
+  // }
 }
-
-export default App;
