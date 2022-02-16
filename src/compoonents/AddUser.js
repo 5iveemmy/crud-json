3154b7f8ba3d394d66cc347aac3637c01a2db758
5iveemmy <82128935+5iveemmy@users.noreply.github.com>
2022-02-17 00:15:48 +0100

    add error for from validation

--- src/compoonents/AddUser.js
+++ src/compoonents/AddUser.js
@@ -1,20 +1,34 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 export const AddUser = ({ onAdd }) => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   const handleOnSubmit = (e) => {
     e.preventDefault();
     onAdd(e.target.name.value, e.target.email.value);
     e.target.name.value = "";
     e.target.email.value = "";
-    };
-    
-    return (
-        <form onSubmit={handleOnSubmit}>
-            <h3>Add User</h3>
-            <input placeholder="Name" name="name" />
-            <input placeholder="Email" name="email" />
-            <button onSubmit={handleOnSubmit}>Add</button>
-            <hr />
-        </form>
-    )
+  };
+
+  return (
+    <form onSubmit={handleOnSubmit && handleSubmit(onSubmit)}>
+      <h3>Add User</h3>
+      <input placeholder="Name" name="name" />
+      {/* <input placeholder="Email" name="email" /> */}
+      <input placeholder="Email" {...register("email", { required: true })} />
+      {errors.email?.type === "required" && "Email is required"}
+
+      <button onSubmit={handleOnSubmit}>Add</button>
+      <hr />
+    </form>
+  );
 };
