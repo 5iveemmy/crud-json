b0efa0bfab9b0c7210f2852f6550493c60452d95
5iveemmy <82128935+5iveemmy@users.noreply.github.com>
2022-02-16 23:26:30 +0100

    create User and AddUser files

--- src/App.js
+++ src/App.js
@@ -13,19 +13,84 @@ export default function App() {
       .catch((error) => console.log(error));
   };
 
-  // const onAdd = aync (name, email) => {
-  //   await fetch("https://jsonplaceholder.typicode.com/users", {
-  //     method: 'POST',
-  //     body: JSON.stringify({
-  //       name: name,
-  //       email: email
-  //     }),
-  //     headers: {
-  //       "Content-type": "applcation/json; charset=UTF-8"
-  //     }
-  //   })
-  //     .then((response) => {
+  const onAdd = async (name, email) => {
+    await fetch("https://jsonplaceholder.typicode.com/users", {
+      method: "POST",
+      body: JSON.stringify({
+        name: name,
+        email: email,
+      }),
+      headers: {
+        "Content-type": "applcation/json; charset=UTF-8",
+      },
+    })
+      .then((response) => {
+        if (response.status !== 201) {
+          return;
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        setUsers((users) => [...users, data]);
+      })
+      .catch((error) => console.log(error));
+  };
 
-  //     })
-  // }
+  const onEdit = async (id, name, email) => {
+    await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        name: name,
+        email: email,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => {
+        if (response.status !== 200) {
+          return;
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        const updatedUsers = users.map((user) => {
+          if (user.id === id) {
+            user.name = name;
+            user.email = email;
+          }
+
+          return user;
+        });
+
+        setUsers((users) => updatedUsers);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const onDelete = async (id) => {
+    await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (response.status !== 200) {
+          return;
+        } else {
+          setUsers(
+            users.filter((user) => {
+              return user.id !== id;
+            })
+          );
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div>
+      <h1>Users</h1>
+    </div>
+  );
 }
